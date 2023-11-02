30466e24f337d0a568f372efd5556285e0e4bcdf
MomentQYC <62551256+MomentQYC@users.noreply.github.com>
2023-11-02 22:52:57 +0800

    fix: Use the instance host instead of the current host

--- packages/frontend/src/config.ts
+++ packages/frontend/src/config.ts
@@ -8,17 +8,33 @@ import { miLocalStorage } from '@/local-storage.js';
 const address = new URL(location.href);
 const siteName = document.querySelector<HTMLMetaElement>('meta[property="og:site_name"]')?.content;
 
-export const host = address.host;
+// export const host = address.host;
 export const hostname = address.hostname;
 export const url = address.origin;
 export const apiUrl = url + '/api';
+
+export let host;
+export let instanceName;
+
+fetch(apiUrl + '/meta', {
+	method: 'POST',
+	body: JSON.stringify({ detail: false }),
+	headers: {
+		'Content-Type': 'application/json'
+	}
+}).then( response => {
+	return response.json();
+} ).then( res => {
+	const data = res.uri;
+	host = data.replace(/^https?:\/\//, '')
+	instanceName = siteName === 'Misskey' ? host : siteName
+} );
 export const wsUrl = url.replace('http://', 'ws://').replace('https://', 'wss://') + '/streaming';
 export const lang = miLocalStorage.getItem('lang') ?? 'en-US';
 export const langs = _LANGS_;
 const preParseLocale = miLocalStorage.getItem('locale');
 export let locale = preParseLocale ? JSON.parse(preParseLocale) : null;
 export const version = _VERSION_;
-export const instanceName = siteName === 'Misskey' ? host : siteName;
 export const ui = miLocalStorage.getItem('ui');
 export const debug = miLocalStorage.getItem('debug') === 'true';
 
